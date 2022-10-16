ede5d69c8ca9c93b782f0129bcff637c3574f5c8
Jill "oatmealine" Monoids <oatmealine@disroot.org>
2022-10-16 23:39:22 +0300

    players module

---
+++ stdlib/players.lua
@@ -0,0 +1,27 @@
+local max_pn = 8
+
+---@type Player[]
+P = {}
+-- for type declarations
+---@type Player
+P1 = nil
+---@type Player
+P2 = nil
+---@type Player
+P3 = nil
+---@type Player
+P4 = nil
+---@type Player
+P5 = nil
+---@type Player
+P6 = nil
+---@type Player
+P7 = nil
+---@type Player
+P8 = nil
+
+for pn = 1, max_pn do
+  local player = SCREENMAN('PlayerP' .. pn)
+  oat['P' .. pn] = player
+  P[pn] = player
+end
