6d75cade2534b3f41a7d8d76c97489f9ae76bf4d
Jill "oatmealine" Monoids <oatmealine@disroot.org>
2022-10-16 23:48:16 +0300

    make eternalfile delete the notedata

--- stdlib/eternalfile.lua
+++ stdlib/eternalfile.lua
@@ -1,4 +1,13 @@
+require('stdlib.players')
+
 return function()
+  if P1 then
+    P1:SetNoteDataFromLua({})
+  end
+  if P2 then
+    P2:SetNoteDataFromLua({})
+  end
+
   function uranium.update()
     if b >= 1 then
       GAMESTATE:SetSongBeat(b % 1)
