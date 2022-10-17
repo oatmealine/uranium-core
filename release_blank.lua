70206204960a13bda1d1414fe517b09156344d36
Jill "oatmealine" Monoids <oatmealine@disroot.org>
2022-10-17 13:13:32 +0300

    release info setup

---
+++ release_blank.lua
@@ -0,0 +1,12 @@
+-- If fields of this are missing, you likely forgot to run update-release.sh
+-- If they're missing and you're an end user, you likely downloaded an unstable release through the repository
+
+return {
+  branch = 'unknown',
+  commit = 'unknown',
+  version = 'unknown',
+
+  name = 'uranium',
+  prettyName = 'Uranium Template',
+  homeURL = 'https://git.oat.zone/oat/uranium-template'
+}
